5a9946d9510dab01c4e77ed75c793b68bbdf73ec
nightmare01979 <johnpatrictromero23@gmail.com>
2026-05-11 11:15:05 +0800

    backup 2026-05-11 11:15

---
+++ .config/hypr/env.lua
@@ -0,0 +1,9 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_SIZE", "20")
